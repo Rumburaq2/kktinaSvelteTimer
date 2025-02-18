fe0c5f511477031b984f887303e45b1288976e12
Rumburaq2 <oliver.hurt@student.gyarab.cz>
2025-02-18 15:06:07 +0100

    init svelte 4 stopwatch rewrite

---
+++ src/SLAcontainer.svelte
@@ -0,0 +1,11 @@
+<script>
+	import SLAbar from "./lib/SLAbar.svelte";
+
+    let isVisible = true;
+</script>
+
+<div class="grid-gap">
+		<div style:display={isVisible ? 'block' : 'none'}>
+			<SLAbar/>
+		</div>
+</div>
